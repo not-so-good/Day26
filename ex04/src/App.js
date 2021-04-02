f4e0d1c47ff1aa54e4ebea92a07638005d47c1e5
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-02 16:37:36 +0200

    ex04 Axios - GET with headers

---
+++ ex04/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import GetRequstSetHeaders from './GetRequestSetHeaders';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequstSetHeaders />
+    </div>
+  );
+}
+
+export default App;
