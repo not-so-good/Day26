c3764b234d451348bbb35899cdb131812b02577f
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-02 16:36:58 +0200

    ex03 Axios - GET error handling

---
+++ ex03/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import GetRequstErrorHandling from './GetRequestErrorHandling';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequstErrorHandling />
+    </div>
+  );
+}
+
+export default App;
