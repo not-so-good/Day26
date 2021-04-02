a6746c45bc9e91ead384b2ce93b8fb0d846de758
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-02 16:34:52 +0200

    ex01 Axios - GET simple request

---
+++ ex01/src/App.js
@@ -0,0 +1,13 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import GetRequst from "./GetRequest";
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequst />
+    </div>
+  );
+}
+
+export default App;
