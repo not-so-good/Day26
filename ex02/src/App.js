629258fb4e7ba05804eca01a873dc21728211f1d
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-02 16:35:34 +0200

    ex02 Axios - GET async/await

---
+++ ex02/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import GetRequstAsyncAwait from './GetRequestAsyncAwait';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequstAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
